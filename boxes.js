9ce1d57f83eebc490c4ed89a18d16f3fdd56820a
ChipMorris10 <goldcomac@mac.com>
2015-07-29 08:38:47 -0600

    Made changes for consistancy throughout the document

--- boxes.js
+++ boxes.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+
+// ----------- T A S K 1 -----------
+$( document ).ready(function() {
+console.log( "The document is ready!" );
+
+// ----------- T A S K 1 -----------
+// Call jQuery
+// Find the <id="secretBox"> and change background to white
+//
+$( '#secretBox').css( {
+  'background-color' : 'white'
+});
+
+
+
+
+}); // end of document
